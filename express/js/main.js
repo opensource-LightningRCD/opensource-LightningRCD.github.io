a5d035918181cede885aaaf6f8bf30546b8f44e8
KandaGiGi <3255596710@qq.com>
2023-04-22 21:20:28 +0800

    a

---
+++ express/js/main.js
@@ -0,0 +1,14 @@
+var artist;
+fetch("../archiver_dataspace/storage/myprofile.json")
+    .then((response) => response.json())
+    .then((json) => {
+        for (let i = 0; i < json.artist.length; i++) {
+            artist += json.artist[i]+", "
+        }
+        artist = artist.replaceAll("undefined", "")
+        document.getElementById("Artist").innerText = artist;
+        console.log(artist)
+    });
+
+document.getElementById("href_home").href = "http://"+window.location.host+"/express";
+document.getElementById("href_arti").href = "http://"+window.location.host+"/express/discover"
